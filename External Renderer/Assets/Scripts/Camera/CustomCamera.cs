7b794a4afa48e02c645284d61b0ea265d2c9ea92
Aidan Pinard <31970261+aidanpinard@users.noreply.github.com>
2021-06-16 12:53:27 -0400

    External renderer dev (#12)
    Implemented Importer and Testing Script.

--- External Renderer/Assets/Scripts/Camera/CustomCamera.cs
+++ External Renderer/Assets/Scripts/Camera/CustomCamera.cs
@@ -1,118 +1,183 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 public class CustomCamera : MonoBehaviour
 {
-    private Camera cam;
-    private Coroutine _screenshotCoroutine;
+    /// <summary>
+    /// Camera component connected to the current GameObject.
+    /// Used for rendering.
+    /// </summary>
+    private Camera _camera;
 
-    private string generateScreenshotPath(string folder)
+    /// <summary>
+    /// Renderer coroutine used to take continuous screenshots at intervals.
+    /// Used as a parameter for StopCoroutine.
+    /// </summary>
+    private Coroutine _rendererCoroutine;
+
+    private void OnEnable()
+    {
+        SaveCamera();
+        if (_camera == null)
+        {
+            enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// Create an internal reference to the Camera component attached to
+    /// this Gameobject. 
+    /// </summary>
+    public void SaveCamera()
+    {
+        _camera = GetComponent<Camera>();
+
+        if (_camera == null)
+        {
+            Debug.LogError("Could not find camera");
+        };
+    }
+
+    /// <summary>
+    /// Generate a valid rendered image output path. 
+    /// </summary>
+    /// <param name="folder">The folder in which to save the image. If 
+    /// invalid, defaults to current directory.</param>
+    /// <returns>Returns a string in the format of
+    /// [<paramref name="folder"/>]/Screenshot-[DateTime].png</returns>
+    private string GenerateRenderPath(string folder)
     {
         try
         {
-            // will throw exception if bad
-            var dir = new DirectoryInfo(folder);
-            if (!dir.Exists)
+            // This will throw an exception if the
+            // folder is invalid
+            DirectoryInfo directory = new DirectoryInfo(folder);
+            if (!directory.Exists)
             {
-                dir.Create();
+                directory.Create();
             }
-            folder = dir.FullName + Path.DirectorySeparatorChar;
+            folder = directory.FullName + Path.DirectorySeparatorChar;
         }
         catch (System.Exception e)
         {
             Debug.LogErrorFormat("Invalid Path \"{0}\". \n Threw {1}.",
-                folder, e);
-            folder = Path.GetFullPath("./");
+                folder, e.Message);
+            // Default to current Directory if provided one is invalid.
+            folder = Directory.GetCurrentDirectory();
         }
+
         return string.Format("{1}Screenshot-{0:yyyy-MM-dd_HH-mm-ss}.png",
                 System.DateTime.Now, folder);
     }
 
-    private void takeScreenshot(string folder, Vector2Int screenshotSize)
+    /// <summary>
+    /// Renders the current view of the Camera.
+    /// </summary>
+    /// <param name="folder">The folder in which to save the render.</param>
+    /// <param name="renderSize">The resolution of the rendered image.</param>
+    public void RenderImage(string folder = "", 
+        Vector2Int renderSize = default(Vector2Int))
     {
-        cam.enabled = false;
-        var renderTexture = new RenderTexture(screenshotSize.x, screenshotSize.y, 24);
-        cam.targetTexture = renderTexture;
+        // ensure screenshot size is at least 300x300 in size.
+        renderSize.Clamp(
+            new Vector2Int(300, 300), 
+            new Vector2Int(int.MaxValue, int.MaxValue));
+
+        _camera.enabled = false;
+        RenderTexture renderTexture = new RenderTexture(renderSize.x, renderSize.y, 24);
+        _camera.targetTexture = renderTexture;
 
         // Render the camera's view.
-        cam.Render();
+        _camera.Render();
         RenderTexture.active = renderTexture;
 
         // Make a new texture and read the active Render Texture into it.
-        Texture2D image = new Texture2D(screenshotSize.x, screenshotSize.y, TextureFormat.RGB24, false);
-        image.ReadPixels(new Rect(0, 0, screenshotSize.x, screenshotSize.y), 0, 0);
+        Texture2D image = new Texture2D(renderSize.x, renderSize.y, TextureFormat.RGB24, false);
+        image.ReadPixels(new Rect(0, 0, renderSize.x, renderSize.y), 0, 0);
         image.Apply();
 
         Destroy(renderTexture);
 
         // Replace the original active Render Texture.
-        cam.targetTexture = null;
+        _camera.targetTexture = null;
         RenderTexture.active = null;
-        cam.enabled = true;
+        _camera.enabled = true;
 
         // now image holds the image in texture2d form
         byte[] png = ImageConversion.EncodeToPNG(image);
 
-        var filename = generateScreenshotPath(folder);
+        // create a filename for the render
+        string filename = GenerateRenderPath(folder);
 
         File.Open(filename, FileMode.OpenOrCreate).Close();
         File.WriteAllBytes(filename, png);
         Debug.LogFormat("Screenshot saved to {0}.", filename);
     }
 
-    IEnumerator screenshotCoroutine(string folder, Vector2Int screenshotSize, float delay)
+    /// <summary>
+    /// Coroutine that renders an image every 
+    /// [<paramref name="delay"/>] seconds.
+    /// </summary>
+    /// <param name="folder">Folder in which to save the render.</param>
+    /// <param name="renderSize">The resolution of the rendered image.</param>
+    /// <param name="delay">The interval between each render.</param>
+    /// <returns></returns>
+    private IEnumerator RendererCoroutine(string folder, 
+        Vector2Int renderSize, float delay)
     {
         while (true)
         {
-            takeScreenshot(folder, screenshotSize);
+            RenderImage(folder, renderSize);
             yield return new WaitForSecondsRealtime(delay);
         }
     }
 
-
-    public void SaveCamera()
+    /// <summary>
+    /// Render the view of the camera repeatedly.
+    /// </summary>
+    /// <param name="delay">The interval between each render.</param>
+    /// <param name="folder">Folder in which to save the render.</param>
+    /// <param name="renderSize">The resolution of the rendered image.</param>
+    public void StartIntervalRendering(float delay = 2f, string folder = "",
+        Vector2Int renderSize = default(Vector2Int))
     {
-        cam = this.GetComponent<Camera>();
-        if (cam == null) Debug.LogError("broke");
+        _rendererCoroutine = 
+            StartCoroutine(RendererCoroutine(folder, renderSize, delay));
     }
 
-    private void OnEnable()
-    {
-        SaveCamera();
-        if (cam == null)
+    /// <summary>
+    /// Stop rendering the view of the camera if StartIntervalRendering 
+    /// was called.
+    /// </summary>
+    public void StopIntervalRendering() {
+        if (_rendererCoroutine != null)
         {
-            this.GetComponent<CustomCamera>().enabled = false;
-        }
-    }
-
-
-    public void Screenshot(string folder, Vector2Int screenshotSize = default(Vector2Int))
-    {
-        if (screenshotSize.x > 300 && screenshotSize.y > 300)
-        {
-            // Set default size
-            screenshotSize = new Vector2Int(Screen.width, Screen.height);
-        }
-
-        takeScreenshot(folder, screenshotSize);
-    }
-
-    public void TakeContinuousScreenshots(string folder, 
-        Vector2Int screenshotSize = default(Vector2Int), float delay = 2f)
-    {
-        _screenshotCoroutine = 
-            StartCoroutine(screenshotCoroutine(folder, screenshotSize, delay));
-    }
-
-    public void StopContinuousScreenshots() {
-        if (_screenshotCoroutine != null)
-        {
-            StopCoroutine(_screenshotCoroutine);
-        } else
+            StopCoroutine(_rendererCoroutine);
+        } 
+        else
         {
             Debug.Log("No Screenshot Coroutine is active.");
         }   
     }
+
+    /// <summary>
+    /// On runtime load, attach the customCamera to the first gameobject
+    /// found with a camera component.
+    /// </summary>
+    [RuntimeInitializeOnLoadMethod]
+    public static void AttachToCamera()
+    {
+        Camera cam = FindObjectOfType<Camera>();
+
+        if (cam != null)
+        {
+            // add this behaviour
+            cam.gameObject.AddComponent<CustomCamera>();
+            return;
+        }
+
+        // If cam is null, then no cameras were found.
+        Debug.LogError("Missing Camera!");
+    }    
 }
---
+++ External Renderer/Assets/Scripts/ImportScene.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace SceneStateExporter
+{
+#if UNITY_EDITOR
+    [CustomEditor(typeof(ImportScene))]
+    public class QuickEditorImport : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            EditorGUILayout.HelpBox("Click Import Now to Import now.", MessageType.None);
+
+            ImportScene currentImporter = target as ImportScene;
+            if (GUILayout.Button("Import Now"))
+            {
+                currentImporter.ImportCurrentScene();
+            }
+        }
+    }
+#endif
+
+    public class ImportScene : MonoBehaviour
+    {
+        public string ImportFilePath = @"D:\Virtana\obj.json";
+        public string ImageSaveFolder = @"D:\Virtana\Planning";
+
+        // Represents when the scene was exported.
+        // if null, then no import has occured
+        public DateTime? ExportTimestamp;
+
+        // TODO: 
+        // possibly implement a pause engine state here
+        // add debug mode with file (and automatic filename checking)
+        // check if file exists
+        // try some sort of better error handling + make a CustomSerialisation
+        // settings to Log the error and later ping a server
+        public void ImportCurrentScene()
+        {
+            Debug.Log("Beginning Import.");
+            
+            // get all current items in scene
+            Scene currentScene = SceneManager.GetActiveScene();
+            List<GameObject> importObjects = new List<GameObject>();
+            currentScene.GetRootGameObjects(importObjects);
+            if (importObjects == null || importObjects.Count == 0)
+            {
+                Debug.LogWarning("Empty object List.");
+                return;
+            }
+
+            foreach (GameObject importObject in importObjects)
+            {
+                importObject.transform.SetParent(transform, true);
+            }
+
+            string json = System.IO.File.ReadAllText(ImportFilePath);
+
+            Debug.Log("Deserializing...");
+
+            SceneState state = JsonConvert.DeserializeObject<SceneState>(json);
+ 
+            if (state == null)
+            {
+                Debug.LogError("Failed to serialize!");
+                return;
+            }
+
+            state.sceneRoot.UnpackData(transform);
+            ExportTimestamp = state.exportDate;
+
+            // put items back in place
+            foreach (GameObject importObject in importObjects)
+            {
+                importObject.transform.parent = null;
+            }
+            Debug.LogFormat("Imported state from {0}! It was generated at {1}", 
+                ImportFilePath, ExportTimestamp);
+
+            FindObjectOfType<CustomCamera>()
+                .RenderImage(ImageSaveFolder, new Vector2Int(1920,1080));
+        }
+    }
+
+    public partial class ObjectState
+    {
+        public void UnpackData(Transform transform)
+        {
+            // update transforms
+            transform.position = ObjectTransform.Position;
+            transform.rotation = ObjectTransform.Rotation;
+            transform.localScale = ObjectTransform.Scale;
+
+            foreach (ObjectState child in Children)
+            {
+                var childTransform = transform.Find(child.Name);
+                if (childTransform == null)
+                {
+                    Debug.LogWarningFormat("Child {0} missing from {1}.",
+                        child.Name, transform.name);
+                }
+                else
+                {
+                    child.UnpackData(childTransform);
+                }
+            }
+        }
+    }
+}
