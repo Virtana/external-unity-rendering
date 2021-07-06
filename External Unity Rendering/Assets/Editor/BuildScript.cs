c1198fbf02b7fd552b43563fa91998f3e50430a8
Aidan Pinard <31970261+aidanpinard@users.noreply.github.com>
2021-07-05 22:49:04 -0400

    Updated Build Script for command line args.
    Signed-off-by: Aidan Pinard <31970261+aidanpinard@users.noreply.github.com>

--- External Unity Rendering/Assets/Editor/BuildScript.cs
+++ External Unity Rendering/Assets/Editor/BuildScript.cs
@@ -1,30 +1,59 @@
 ﻿using UnityEngine;
 using System.IO;
 using UnityEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class BuildScript : MonoBehaviour
 {
-    public static void BuildPhysics()
+    // add a better way to detect options
+    private static readonly Dictionary<string, BuildConfigurations> _buildSymbols = new Dictionary<string, BuildConfigurations>
     {
-        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "PHYSICS" );
-        Build("Physics");
+        { "-physics", BuildConfigurations.Physics },
+        { "-renderer", BuildConfigurations.Renderer },
+    };
+
+    private enum BuildConfigurations
+    {
+        Physics = 1,
+        Renderer = 2
     }
 
-    public static void BuildRenderer()
+    public static void Build()
     {
-        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "RENDERER");
-        Build("Renderer");
-    }
+        // Filter unity's command line args
+        string[] args = Environment.GetCommandLineArgs();
 
-    static void Build(string buildname)
-    {
-        string[] scenePaths = Directory.GetFiles(Application.dataPath, "*.unity", SearchOption.AllDirectories);
+        // Make renderer by default
+        BuildConfigurations config = 0;
+
+        foreach (string arg in args)
+        {
+            _buildSymbols.TryGetValue(arg, out config);
+        }
+
+        if (config == 0)
+        {
+            Console.WriteLine("Missing Argument for build type.");
+            Console.ResetColor();
+            EditorApplication.Exit(1);
+        }
+
+        string[] scenePaths = Directory.GetFiles(Application.dataPath,
+            "*.unity", SearchOption.AllDirectories);
+
         for (int i = 0; i < scenePaths.Length; ++i)
         {
             scenePaths[i] = scenePaths[i].Remove(0, Application.dataPath.Length - 6);
         }
+
+        string outputName = Enum.GetName(typeof(BuildConfigurations), config);
+
         BuildPipeline.BuildPlayer(scenePaths,
-            Path.GetFullPath(Path.Combine(Application.dataPath, "../../", buildname)),
+            Path.GetFullPath(Path.Combine(Application.dataPath,
+                "..", "..", "builds", outputName, outputName)
+            ),
             BuildTarget.StandaloneLinux64, BuildOptions.None);
     }
 }
