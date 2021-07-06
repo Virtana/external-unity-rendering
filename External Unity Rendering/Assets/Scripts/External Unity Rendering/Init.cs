65b38388167eef7268f6898b1fc868948ce6960d
Aidan Pinard <31970261+aidanpinard@users.noreply.github.com>
2021-07-06 07:55:09 -0400

    Completed demo build script for physics instance.
    Added Exit method, wrapping Quit and exit message.
    Added a demo formatter for the logging.
    Added Hacky time delay before quit (see comments).
    Disable all cameras on load.
    Added exporter choices, directories, resolution arguments.
    Added comment for init if not unity editor.
    
    Signed-off-by: Aidan Pinard <31970261+aidanpinard@users.noreply.github.com>

--- External Unity Rendering/Assets/Scripts/External Unity Rendering/Init.cs
+++ External Unity Rendering/Assets/Scripts/External Unity Rendering/Init.cs
@@ -1,8 +1,17 @@
-﻿using UnityEngine;
+﻿//#define PHYSICS
+//#define RENDERER
+using UnityEngine;
 using ExternalUnityRendering;
+using ExternalUnityRendering.PathManagement;
 using System;
 using System.Threading.Tasks;
+using System.Linq;
 
+// cant use init script functionality in editor play mode
+// use editor gui functions instead
+
+// Set to !UNITY_EDITOR when not testing code
+#if UNITY_2017_1_OR_NEWER //!UNITY_EDITOR
 public class Init : MonoBehaviour
 {
     // TODO Initialize list
@@ -14,10 +23,39 @@ public class Init : MonoBehaviour
     // may need to add a way to check for physics terminating then
     // check for importer being done then exit (throw signal or something??)
 
-    private static void FailAndQuit(string errorMessage)
+    private static void Exit(string message, int exitCode = 1)
     {
-        Debug.LogError(errorMessage);
-        Application.Quit(1);
+        if (exitCode != 0)
+        {
+            Debug.LogError(message);
+        }
+        else
+        {
+            Debug.Log(message);
+        }
+        Application.Quit(exitCode);
+    }
+
+    private static void Application_logMessageReceived(string message, string stacktrace, LogType logType)
+    {
+        switch (logType)
+        {
+            case LogType.Assert:
+            case LogType.Error:
+            case LogType.Exception:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine($"{Enum.GetName(typeof(LogType), logType)}: ${message}.\n{stacktrace}");
+                break;
+            case LogType.Warning:
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Error.WriteLine($"{Enum.GetName(typeof(LogType), logType)}: ${message}.\n{stacktrace}");
+                break;
+            case LogType.Log:
+                Console.WriteLine($"LOG: {message}");
+                break;
+        }
+
+        Console.ResetColor();
     }
 
 #if PHYSICS
@@ -29,7 +67,7 @@ public class Init : MonoBehaviour
         {
             if (time.Length < 2)
             {
-                FailAndQuit("Invalid Time Input");
+                Exit("Invalid Time Input");
             }
 
             // get last character
@@ -55,7 +93,7 @@ public class Init : MonoBehaviour
                     Milliseconds = 60 * 1000;
                     break;
                 default:
-                    FailAndQuit("Invalid modifier. Valid modifiers are: " +
+                    Exit("Invalid modifier. Valid modifiers are: " +
                         "\n\t'm' : minutes" +
                         "\n\t's' : seconds" +
                         "\n\t' ' : milliseconds");
@@ -64,11 +102,11 @@ public class Init : MonoBehaviour
 
             if (!int.TryParse(time, out int number))
             {
-                FailAndQuit("Failed to parse time. Check the parameters given.");
+                Exit($"Failed to parse time. Given {time}.");
             }
             else if (number < 0)
             {
-                FailAndQuit("Time cannot be less than 0");
+                Exit("Time cannot be less than 0");
             }
             else
             {
@@ -93,13 +131,31 @@ public class Init : MonoBehaviour
 
             await Task.Delay(delay);
         }
+
+        // TODO await transmission complete.
+        await Task.Delay(5000); // HACK wait five seconds to finish transmit
+        Exit("Completed Execution.", 0);
     }
 #endif
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     static void Initialize()
     {
+        string[] commandLineArgs = Environment.GetCommandLineArgs();
+        if (commandLineArgs.Contains("-nographics"))
+        {
+            // TODO change to intercept
+            // https://docs.unity3d.com/ScriptReference/ILogHandler.html
+            // #:~:text=taking%20this%20survey.-,ILogHandler,-interface%20in%20UnityEngine
+            Application.logMessageReceived += Application_logMessageReceived;
+        }
+
 #if PHYSICS
+        foreach (Camera camera in FindObjectsOfType<Camera>())
+        {
+            camera.enabled = false;
+        }
+
         ExportScene exporter = FindObjectOfType<ExportScene>();
         if (exporter == null)
         {
@@ -115,28 +171,103 @@ public class Init : MonoBehaviour
         Time delayArgument = null;
         Time totalTimeArgument = null;
         int totalExportsArgument = -1;
+        ExportScene.PostExportAction exportAction = ExportScene.PostExportAction.Nothing;
+        Vector2Int renderResolution = new Vector2Int();
+        DirectoryManager renderPath = new DirectoryManager();
+        DirectoryManager jsonPath = new DirectoryManager();
 
         // TODO add checks for folder path, export type and output resolution
-        string[] commandLineArgs = Environment.GetCommandLineArgs();
-        for (int i = 0, j = 1; j < commandLineArgs.Length; i++, j++)
+
+        for (int i = 0, j = 1; i < commandLineArgs.Length; i++, j++)
         {
             switch (commandLineArgs[i])
             {
                 case "-delay":
+                    if (j == commandLineArgs.Length)
+                    {
+                        Exit("Missing Delay argument. If it is not needed, exclude it.");
+                    }
                     delayArgument = new Time(commandLineArgs[j]);
                     break;
                 case "-time":
+                    if (j == commandLineArgs.Length)
+                    {
+                        Exit("Missing time argument. If it is not needed, exclude it.");
+                    }
                     totalTimeArgument = new Time(commandLineArgs[j]);
                     break;
                 case "-export":
-                    if (!int.TryParse(commandLineArgs[j], out totalExportsArgument))
+                    if (j == commandLineArgs.Length)
                     {
-                        FailAndQuit("Failed to parse export count.");
+                        Exit("Missing export count argument. If it is not needed, exclude it.");
+                    }
+                    else if (!int.TryParse(commandLineArgs[j], out totalExportsArgument))
+                    {
+                        Exit($"Failed to parse export count. Given {commandLineArgs[j]}");
                     }
                     else if (totalExportsArgument < 1)
                     {
-                        FailAndQuit("Export count is less than 1. No exports will be performed.");
+                        Exit("Export count is less than 1. No exports will be performed.");
                     }
+                    break;
+                case "-h":
+                case "-height":
+                    if (j == commandLineArgs.Length)
+                    {
+                        Exit("Missing height argument.");
+                    }
+                    else if (!int.TryParse(commandLineArgs[j], out int height))
+                    {
+                        Exit($"Failed to parse height. Given {commandLineArgs[j]}");
+                    }
+                    else
+                    {
+                        renderResolution.y = height;
+                    }
+                    break;
+                case "-w":
+                case "-width":
+                    if (j == commandLineArgs.Length)
+                    {
+                        Exit("Missing width argument.");
+                    }
+                    else if (!int.TryParse(commandLineArgs[j], out int width))
+                    {
+                        Exit($"Failed to parse width. Given {commandLineArgs[j]}");
+                    }
+                    else
+                    {
+                        renderResolution.x = width;
+                    }
+                    break;
+                case "-r":
+                case "-renderPath":
+                    if (j == commandLineArgs.Length)
+                    {
+                        Exit("Missing render path argument.");
+                    }
+                    renderPath.Path = commandLineArgs[j];
+                    break;
+                case "-writeToFile":
+                    exportAction |= ExportScene.PostExportAction.WriteToFile;
+                    if (j == commandLineArgs.Length)
+                    {
+                        Exit("Missing path to write json files to.");
+                    }
+                    else if (!commandLineArgs[j].StartsWith("-"))
+                    {
+                        jsonPath.Path = commandLineArgs[j];
+                        if (jsonPath.Path == Application.persistentDataPath)
+                        {
+                            Debug.Log($"Writing to {Application.persistentDataPath}");
+                        }
+                    }
+                    break;
+                case "-logExport":
+                    exportAction |= ExportScene.PostExportAction.Log;
+                    break;
+                case "-transmit":
+                    exportAction |= ExportScene.PostExportAction.Transmit;
                     break;
             }
         }
@@ -144,7 +275,7 @@ public class Init : MonoBehaviour
         if (delayArgument != null && totalExportsArgument < 1 && totalTimeArgument != null
             && delayArgument.Milliseconds * totalExportsArgument != totalTimeArgument.Milliseconds)
         {
-            FailAndQuit("Inconsistent time arguments given.");
+            Exit("Inconsistent time arguments given.");
         }
 
         int totalExports = -1;
@@ -156,30 +287,30 @@ public class Init : MonoBehaviour
                     totalTimeArgument.Milliseconds / delayArgument.Milliseconds;
             if (totalExports < 1)
             {
-                FailAndQuit("Provided delay and total time result in a total export count less than 0.");
+                Exit("Provided delay and total time result in a total export count less than 0.");
             }
 
             delay = delayArgument.Milliseconds;
         }
-        else if (delayArgument != null && totalExportsArgument < 1)
+        else if (delayArgument != null && totalExportsArgument > 0)
         {
             totalExports = totalExportsArgument;
             delay = delayArgument.Milliseconds;
         }
-        else if (totalExportsArgument < 1 && totalTimeArgument != null)
+        else if (totalExportsArgument > 0 && totalTimeArgument != null)
         {
             totalExports = totalExportsArgument;
             delay = totalTimeArgument.Milliseconds / totalExportsArgument;
-
         }
         else
         {
-            FailAndQuit("Missing Arguments to export. ");
+            Exit("Missing Arguments to export.");
         }
 
         // TODO add other options currently partly hardcoded
-        ExportLoop(exporter, delay, totalExports, ExportScene.PostExportAction.Transmit,
-            default, System.IO.Path.GetFullPath("../"));
+        exporter.ExportFolder = jsonPath.Path;
+        ExportLoop(exporter, delay, totalExports, exportAction,
+            renderResolution, renderPath.Path);
 #elif RENDERER
 
 #else
@@ -188,3 +319,4 @@ public class Init : MonoBehaviour
 #endif
     }
 }
+#endif
